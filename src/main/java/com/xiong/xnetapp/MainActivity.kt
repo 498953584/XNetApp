a4762194282ffb0b271696797db4ca222d51a66c
498953584 <498953584@qq.com>
2019-08-02 19:14:30 +0800

    0

--- src/main/java/com/xiong/xnetapp/MainActivity.kt
+++ src/main/java/com/xiong/xnetapp/MainActivity.kt
@@ -3,19 +3,14 @@ package com.xiong.xnetapp
 import android.content.Context
 import android.content.IntentFilter
 import android.content.SharedPreferences
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.net.ConnectivityManager
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-import java.lang.Math.log
-import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,37 +20,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var sp = this.run { getSharedPreferences("SP", Context.MODE_PRIVATE) } as SharedPreferences
         Volume.run {
-            setOnEditorActionListener(TextView.OnEditorActionListener(
-                function = fun(textView: TextView, actionId: Int, event: KeyEvent): Boolean {
-                    try{
-                    if (actionId == EditorInfo.IME_ACTION_SEND ||
-                        (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER &&
-                                event.action == KeyEvent.ACTION_DOWN)
-                    ) {
-                        Toast.makeText(context,"129"+textView.text,Toast.LENGTH_SHORT)
-                        //存入数据
-                        var editor = sp.edit() as SharedPreferences.Editor
-                        editor.putInt("Volume", Integer.parseInt(textView.text.toString()))
-                        editor.commit()
+            setOnEditorActionListener(
+                TextView.OnEditorActionListener(
+                    fun(textView: TextView, actionId: Int, event: KeyEvent?): Boolean {
+                        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                            (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER &&
+                                    event.action == KeyEvent.ACTION_DOWN)
+                        ) {
+                            var volume = Integer.parseInt(textView.text.toString())
+                            if (volume in 0..100) {
+                                //存入数据
+                                var editor = sp.edit() as SharedPreferences.Editor
+                                editor.putInt("Volume", volume)
+                                editor.commit()
+                                var audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+                                audioManager.setStreamVolume(
+                                    AudioManager.STREAM_MUSIC,
+                                    Math.round(volume / 100.0 * audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)).toInt(),
+                                    AudioManager.FLAG_PLAY_SOUND
+                                )
+                                return false
+                            }
+                            return true
+                        }
                         return true
-                    }
-                    return false
-                }
-                catch(ex : Exception){
-                    Toast.makeText(context,ex.message,Toast.LENGTH_LONG)
-                    return false
-                }
-                }
-            ))
+                    })
+            )
+            setText(sp.getInt("Volume", 40).toString())
         }
 
-        var netBroadcastReceiver = NetworkStateReceiver()
-        //实例化意图
-        var filter = IntentFilter()
-        //设置广播的类型
-        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
-        //注册广播，有网络变化的时候会触发onReceive
-        registerReceiver(netBroadcastReceiver, filter)
+//        var netBroadcastReceiver = NetworkStateReceiver()
+//        //实例化意图
+//        var filter = IntentFilter()
+//        //设置广播的类型
+//        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+//        //注册广播，有网络变化的时候会触发onReceive
+//        registerReceiver(netBroadcastReceiver, filter)
         // 设置监听
         //netBroadcastReceiver.setNetEvent(this)
     }
--- src/main/java/com/xiong/xnetapp/NetworkStateReceiver.kt
+++ src/main/java/com/xiong/xnetapp/NetworkStateReceiver.kt
@@ -8,19 +8,36 @@ import android.content.Context.CONNECTIVITY_SERVICE
 import android.content.SharedPreferences
 import android.media.AudioManager
 import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.util.Log
 
 
 class NetworkStateReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         //判断广播的类型为网络action后
-        if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+        if (intent.action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
             var audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-            if (NetworkAvailable(context)) {
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_PLAY_SOUND)
+            var max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+            var current: Double
+//            Log.d("MUSIC", "max : $max current : $current")
+            Log.d("MUSIC", "current : " + audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
+            var sp = context.getSharedPreferences("SP", Context.MODE_PRIVATE) as SharedPreferences
+            if (isNetworkAvailable(context)) {
+                Toast.makeText(context, "网络已连接", Toast.LENGTH_SHORT)
+                current = sp.getInt("Volume", 40) / 100.0 * max
+                audioManager.setStreamVolume(
+                    AudioManager.STREAM_MUSIC,
+                    Math.round(current).toInt(),
+                    AudioManager.FLAG_PLAY_SOUND
+                )
             } else {
-                var sp = context.getSharedPreferences("SP", Context.MODE_PRIVATE) as SharedPreferences
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, sp.getInt("Volume",40), AudioManager.FLAG_PLAY_SOUND)
+                Toast.makeText(context, "无网络连接", Toast.LENGTH_SHORT)
+                var editor = sp.edit() as SharedPreferences.Editor
+                current = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) / max * 100.0
+                editor.putInt("Volume", Math.round(current).toInt())
+                editor.commit()
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_PLAY_SOUND)
             }
         }
     }
@@ -28,18 +45,19 @@ class NetworkStateReceiver : BroadcastReceiver() {
     /**
      * 检测网络是否连接
      */
-    private fun NetworkAvailable(context: Context): Boolean {
+    private fun isNetworkAvailable(context: Context): Boolean {
         try {
             //得到网络连接信息
             var manager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
             // 获取NetworkInfo对象
-            val networkInfo = manager.activeNetworkInfo
+            val networkInfo :NetworkInfo? = manager.activeNetworkInfo
             //去进行判断网络是否连接
-            if (networkInfo != null || networkInfo!!.isAvailable) {
+            if (networkInfo != null && networkInfo.isAvailable) {
                 return true
             }
         } catch (e: Exception) {
             e.printStackTrace()
+
         }
         return false
     }
